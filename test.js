a2263fce504e0a2344ecae10412a196c1695e1cf
S2HP <ubermatiss@gmail.com>
2023-09-06 10:29:05 +0300

    1

---
+++ test.js
@@ -0,0 +1,42 @@
+class Cilveks {
+    constructor(age, name, sex){
+        this.vecums = age;
+        this.vards = name;
+        this.dzimums = sex;
+    }
+    svinetBday(){
+        this.vecums++;
+        this.info();
+    }
+    mainitVardu(jaunaisVards){
+        this.vards = jaunaisVards;
+    }
+    mainitDzimumu(jaunaisDzimums=""){
+        if (jaunaisDzimums == ""){
+            if(this.dzimums == "s"){
+                this.dzimums == "v;"
+            } else {
+                this.dzimums == "s";
+            }
+        } else {
+            this.dzimums = jaunaisDzimums;
+        }
+        this.info();
+    } 
+
+    info(){
+        let teksts = "Sveiki! Mani sauc " + this.vards + "Man ir " + this.vecums + "gadu.";
+        teksts += "Es esmu ";
+        if (this.dzimums == "s") {
+            teksts += "sieviete."; 
+        } else if (this.dzimums == "v") {
+            teksts += "vīrietis.";
+        } else {
+            teksts += this.dzimums
+        }
+        console.log(teksts)
+    }
+}
+
+
+pirmais = new Cilveks(15, "Katrina", "s")
---
+++ test.py
@@ -0,0 +1,3 @@
+from multiprocessing.pool import ApplyResult
+
+
